128519979833f0c85f717d484f5b49c4b5ca298a
anirudh-codes26 <anirudhbaliyan03@gmail.com>
2026-02-01 11:48:16 +0530

    Remove app.options wildcard for Express 5

--- backend/index.js
+++ backend/index.js
@@ -88,7 +88,8 @@ app.use(cors({
 }));
 
 // ✅ REQUIRED
-app.options("*", cors());
+// app.options("/*", cors());
+
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
